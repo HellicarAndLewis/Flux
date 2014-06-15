fa916c4f433b8ff55370ccb10204fc0e2742ae28
Jonas Jongejan <jonas@halfdanj.dk>
2014-06-15 22:30:24 +0200

    check the render radar for if there are colors (otherwise it crashes)

--- FluxOF/src/Animations/RenderRadar.cpp
+++ FluxOF/src/Animations/RenderRadar.cpp
@@ -120,50 +120,52 @@ void RenderRadar::selfUpdate(){
     noiseShader.end();
     noiseTexture.end();
     
-    terrainTransitionTex.swap();
-    terrainTransitionTex.dst->begin();
-    terrainTransition.begin();
-    terrainTransition.getShader().setUniformTexture("backbuffer", *terrainTransitionTex.src, 0);
-    terrainTransition.getShader().setUniformTexture("depthMap", terrainDepthMap, 1);
-    terrainTransition.getShader().setUniformTexture("normalMap", noiseTexture, 2);
-    terrainTransition.getShader().setUniformTexture("radarTex", radarTexture, 3);
-    
-    terrainTransition.getShader().setUniform3f("dstColor1",
-                                               ((float)colorPalette[0].r)/255.0,
-                                               ((float)colorPalette[0].g)/255.0,
-                                               ((float)colorPalette[0].b)/255.0);
-    terrainTransition.getShader().setUniform3f("dstColor2",
-                                               ((float)colorPalette[1].r)/255.0,
-                                               ((float)colorPalette[1].g)/255.0,
-                                               ((float)colorPalette[1].b)/255.0);
-    terrainTransition.getShader().setUniform3f("dstColor3",
-                                               ((float)colorPalette[2].r)/255.0,
-                                               ((float)colorPalette[2].g)/255.0,
-                                               ((float)colorPalette[2].b)/255.0);
-    terrainTransition.getShader().setUniform3f("dstColor4",
-                                               ((float)colorPalette[3].r)/255.0,
-                                               ((float)colorPalette[3].g)/255.0,
-                                               ((float)colorPalette[3].b)/255.0);
-    terrainTransition.getShader().setUniform3f("dstColor5",
-                                               ((float)colorPalette[4].r)/255.0,
-                                               ((float)colorPalette[4].g)/255.0,
-                                               ((float)colorPalette[4].b)/255.0);
-    
-    terrainTransition.getShader().setUniform1f("resolution", terrainResolution);
-    
-    glBegin(GL_QUADS);
-    glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
-    glTexCoord2f(terrainResolution, 0); glVertex3f(terrainResolution, 0, 0);
-    glTexCoord2f(terrainResolution, terrainResolution); glVertex3f(terrainResolution, terrainResolution, 0);
-    glTexCoord2f(0,terrainResolution);  glVertex3f(0,terrainResolution, 0);
-    glEnd();
-    terrainTransition.end();
-    
-    terrainTransitionTex.dst->end();
-    
-    terrainTex.begin();
-    terrainTransitionTex.dst->draw(0, 0);
-    terrainTex.end();
+    if(colorPalette.size()){
+        terrainTransitionTex.swap();
+        terrainTransitionTex.dst->begin();
+        terrainTransition.begin();
+        terrainTransition.getShader().setUniformTexture("backbuffer", *terrainTransitionTex.src, 0);
+        terrainTransition.getShader().setUniformTexture("depthMap", terrainDepthMap, 1);
+        terrainTransition.getShader().setUniformTexture("normalMap", noiseTexture, 2);
+        terrainTransition.getShader().setUniformTexture("radarTex", radarTexture, 3);
+        
+        terrainTransition.getShader().setUniform3f("dstColor1",
+                                                   ((float)colorPalette[0].r)/255.0,
+                                                   ((float)colorPalette[0].g)/255.0,
+                                                   ((float)colorPalette[0].b)/255.0);
+        terrainTransition.getShader().setUniform3f("dstColor2",
+                                                   ((float)colorPalette[1].r)/255.0,
+                                                   ((float)colorPalette[1].g)/255.0,
+                                                   ((float)colorPalette[1].b)/255.0);
+        terrainTransition.getShader().setUniform3f("dstColor3",
+                                                   ((float)colorPalette[2].r)/255.0,
+                                                   ((float)colorPalette[2].g)/255.0,
+                                                   ((float)colorPalette[2].b)/255.0);
+        terrainTransition.getShader().setUniform3f("dstColor4",
+                                                   ((float)colorPalette[3].r)/255.0,
+                                                   ((float)colorPalette[3].g)/255.0,
+                                                   ((float)colorPalette[3].b)/255.0);
+        terrainTransition.getShader().setUniform3f("dstColor5",
+                                                   ((float)colorPalette[4].r)/255.0,
+                                                   ((float)colorPalette[4].g)/255.0,
+                                                   ((float)colorPalette[4].b)/255.0);
+        
+        terrainTransition.getShader().setUniform1f("resolution", terrainResolution);
+        
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0); glVertex3f(0, 0, 0);
+        glTexCoord2f(terrainResolution, 0); glVertex3f(terrainResolution, 0, 0);
+        glTexCoord2f(terrainResolution, terrainResolution); glVertex3f(terrainResolution, terrainResolution, 0);
+        glTexCoord2f(0,terrainResolution);  glVertex3f(0,terrainResolution, 0);
+        glEnd();
+        terrainTransition.end();
+        
+        terrainTransitionTex.dst->end();
+        
+        terrainTex.begin();
+        terrainTransitionTex.dst->draw(0, 0);
+        terrainTex.end();
+    }
 }
 
 void RenderRadar::selfDraw(){
