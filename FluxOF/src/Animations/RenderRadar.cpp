093934569f9c657011407bd365764aaf4ee8f161
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2014-06-18 12:20:05 -0400

    starting w color palette

--- FluxOF/src/Animations/RenderRadar.cpp
+++ FluxOF/src/Animations/RenderRadar.cpp
@@ -303,25 +303,16 @@ void RenderRadar::selfDraw(){
 void RenderRadar::selfDrawOverlay(){
     if(bDebug){
         
-        ofPushMatrix();
-        
-        ofScale(0.5, 0.5);
-        terrainTex.draw(assets->terrainResolution()*0.25,0);
-        
-        ofPushMatrix();
-        ofTranslate(assets->terrainResolution()*1.25,0);
-        shoeTex.dst->draw(0, 0);
-        assets->shoeUVWireframe.draw(0, 0,shoeTex.dst->getWidth(), shoeTex.dst->getHeight());
-        ofPopMatrix();
-        
-        ofPushMatrix();
-        ofScale(0.25, 0.25);
-        assets->terrainDepthMap.draw(0,0);
-        assets->terrainNormalMap.draw(0,assets->terrainResolution());
-        radarTexture.draw(0,assets->terrainResolution()*2.0);
-        ofPopMatrix();
-        
-        ofPopMatrix();
+//        ofPushMatrix();
+//        ofScale(0.5, 0.5);
+//        terrainTex.draw(assets->terrainResolution()*0.25,0);
+//        ofPushMatrix();
+//        ofScale(0.25, 0.25);
+//        assets->terrainDepthMap.draw(0,0);
+//        assets->terrainNormalMap.draw(0,assets->terrainResolution());
+//        radarTexture.draw(0,assets->terrainResolution()*2.0);
+//        ofPopMatrix();
+//        ofPopMatrix();
         
 //        audioTex.draw(0,assets->terrainResolution()*0.5,assets->terrainResolution()*0.5,10);
         
@@ -330,11 +321,17 @@ void RenderRadar::selfDrawOverlay(){
         
         ofPushMatrix();
         ofPushStyle();
-        ofTranslate(ofGetWidth()-margin*1.5-paletteSize,0);
+        
+        ofTranslate(ofGetWidth()-assets->terrainResolution()*0.5,0);
+        shoeTex.dst->draw(0, 0);
+        assets->shoeUVWireframe.draw(0, 0,shoeTex.dst->getWidth(), shoeTex.dst->getHeight());
+        
+        ofTranslate(-margin*1.5-paletteSize,0);
         for(int i = 0; i < dstPalette.size(); i++){
             ofSetColor(dstPalette[i]);
             ofCircle(margin, i*(paletteSize*2.0+5)+paletteSize*0.5+5, paletteSize);
         }
+        
         ofPopStyle();
         ofPopMatrix();
     }
