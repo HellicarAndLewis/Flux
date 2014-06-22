d3cfe6eba880aba838ba8ccb05764f9992e4e8fb
Jonas Jongejan / Flux <jonas@halfdanj.dk>
2014-06-22 17:14:00 +0100

    audio trigger osc

--- FluxOF/src/AudioTrigger.cpp
+++ FluxOF/src/AudioTrigger.cpp
@@ -1,7 +1,7 @@
 #include "AudioTrigger.h"
 
 void AudioTrigger::setup(){
-    
+    osc.setup("localhost", 8888);
 }
 
 void AudioTrigger::setupUI(){
@@ -9,5 +9,9 @@ void AudioTrigger::setupUI(){
 }
 
 void AudioTrigger::guiEvent(ofxUIEventArgs &e){
-    
+    if(e.getName() == "test"){
+        ofxOscMessage msg;
+        msg.setAddress("/test");
+        msg.addFloatArg(e.getSlider()->getValue() );
+    }
 }
